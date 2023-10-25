51b4ee655fab920948e695051874d7d9db5528ed
Ishant Sharma <heyy.ishant@gmail.com>
2023-10-25 03:40:46 +0000

    Done Successfully

---
+++ 01-Basics/01-Variables.js
@@ -0,0 +1,18 @@
+const accountId = 226727955934
+let accountEmail = "Ishan@gmail.com"
+var accountPassword = "975658" 
+accountCity = "Aligarh"
+let accountState;
+
+// accountId = 588426      // Not Allowed
+
+// prefer not to use var keyword 
+
+accountEmail = "hello.ishan@google.com"
+accountPassword = "120120"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
