cd8817cf71697842ef7e4c6d0fbf1a818ba4ac43
Ishant Sharma <heyy.ishant@gmail.com>
2023-10-26 13:16:31 +0000

    Datatypes recall

---
+++ 01-Basics/03-conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = "Ishan"
+
+
+// let firstName = "Ishan";
+
+
+// console.log(typeof (score));
+// console.log(typeof firstName);
+
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"200" => 200
+//"200abc" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = "Ishan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true;  0 => false
+//"" => false
+//"Ishan" => true
+
+let aNumber = 45
+
+let convertedNumber = String(aNumber)
+
+console.log(convertedNumber);
+console.log(typeof convertedNumber);
+
