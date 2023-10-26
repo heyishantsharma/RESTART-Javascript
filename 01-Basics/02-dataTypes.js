6abeabd3ed736f8c9c2554d2a290b16c86603f36
Ishant Sharma <heyy.ishant@gmail.com>
2023-10-26 08:19:18 +0000

    datatypes

---
+++ 01-Basics/02-dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";  // treat all js code as a newer version
+
+// alert(3 + 3) we are using node js not browser
+
+let name = "Ishan"
+let age = 20;
+let isLoggedIn = false
+
+//number => 2 to power 53
+//string => ""
+// Boolean =>True/False
+//Null => stand alone value
+//Undefined =>
+//symbol => unique
+
+// object
+
+console.log(typeof null); // Object
+console.log(typeof undefined); // undefined
